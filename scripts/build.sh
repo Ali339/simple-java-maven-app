3acb509c0db82e34a63647be82a14ae36441d92a
root <root@jenkins.linoop.com>
2020-01-19 15:33:49 -0500

    whatever

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ echo **********BUILDING JAR**************
 
 echo *************************************
 
-docker run --rm  -v  /root/simple-java-maven-app:/usr/src/mymaven -v /root/.m2/:/root/.m2/ -w /usr/src/mymaven  maven:3.3-jdk-8  mvn -DskipTests clean package
+docker run --rm  -v  /var/lib/docker/volumes/jenkins-data/_data/workspace/mypipeline:/usr/src/mymaven -v /root/.m2/:/root/.m2/ -w /usr/src/mymaven  maven:3.3-jdk-8  mvn -DskipTests clean package
